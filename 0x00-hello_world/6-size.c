75684e564ea1287f974ae176be8c7774912da53f
elishambadi <embadi43@gmail.com>
2022-04-07 16:47:49 +0000

    view compile in 32 and 64 bit machines v3

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 
 /**
- *main - does main stuffs 
+ *main - does main stuffs
  *
  * Return: 0 to terminate program
  */
-int main(void){
+int main(void)
+{
 	int int_1;
 	char char_1;
 	long long_1;
 	long long long_long_1;
 	float float_1;
+
 	printf("Size of a char: %d byte(s)\n", sizeof(char_1));
 	printf("Size of an int: %d byte(s)\n", sizeof(int_1));
 	printf("Size of a long int: %d byte(s)\n", sizeof(long_1));
