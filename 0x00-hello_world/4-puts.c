c83ec31fb421abc5ba083619e3ac6151f82574a1
elishambadi <embadi43@gmail.com>
2022-04-07 16:50:37 +0000

    add betty coding style to task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
-int main(){
+/**
+ *main - main function
+ *
+ * Return: 0 to terminate application
+ */
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
