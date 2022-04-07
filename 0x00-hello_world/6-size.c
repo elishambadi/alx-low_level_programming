87efdea94baadf6882ef6cffb537f648da2c7f93
elishambadi <embadi43@gmail.com>
2022-04-07 16:45:59 +0000

    view compile in 32 and 64 bit machines v2

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,10 +11,10 @@ int main(void){
 	long long_1;
 	long long long_long_1;
 	float float_1;
-	printf("Size of a char: %d bytes(s)\n", sizeof(char_1));
-	printf("Size of an int: %d bytes(s)\n", sizeof(int_1));
-	printf("Size of a long int: %d bytes(s)\n", sizeof(long_1));
-	printf("Size of a long long int: %d bytes(s)\n", sizeof(long_long_1));
-	printf("Size of a float: %d bytes(s)\n", sizeof(float_1));
+	printf("Size of a char: %d byte(s)\n", sizeof(char_1));
+	printf("Size of an int: %d byte(s)\n", sizeof(int_1));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long_1));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(long_long_1));
+	printf("Size of a float: %d byte(s)\n", sizeof(float_1));
 	return (0);
 }
