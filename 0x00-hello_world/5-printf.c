cebd98ca5a51d40427e3b4e515b8406b284117fa
elishambadi <embadi43@gmail.com>
2022-04-07 16:19:50 +0000

    modify to follow betty coding and documentation styel

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
-int main(){
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+/*Main function*/
+int main(void)
+{
+	printf ("with proper grammar, but the outcome is a piece of art,\n");
 	return(0);
 }
