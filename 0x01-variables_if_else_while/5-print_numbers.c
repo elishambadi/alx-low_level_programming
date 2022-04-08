963949764e2a3a611736abaf940b11d9d01f5cc7
elishambadi <embadi43@gmail.com>
2022-04-08 23:06:23 +0000

    add code to print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *main - main function
+ *
+ *Return: 0 to terminate program
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		printf("%d", i);
+		i++;
+	}
+	printf("\n");
+	return (0);
+}
