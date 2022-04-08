58ebf56e56e1014ed02e3417a3042939cb42e147
elishambadi <embadi43@gmail.com>
2022-04-08 15:46:52 +0000

    add commanfs for task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,7 +9,7 @@
 int main(void)
 {
 	char alpha[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
-i		'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+		'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
 		's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 
 	int i = 0;
@@ -18,7 +18,12 @@ i		'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
 	{
 		char b = alpha[i];
 
-		printf("%c", b);
+		
+		if ( b != 'q' && b != 'e')
+		{
+			printf("%c", b);
+		}
+
 		i++;
 	}
 	printf("\n");
