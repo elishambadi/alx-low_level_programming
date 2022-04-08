230400ed5e17fddd813625b477d9a6998f6d2a10
elishambadi <embadi43@gmail.com>
2022-04-08 15:28:16 +0000

    add task 3 code

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ *main - main function
+ *
+ *Return: 0 to terminate programme
+ *
+ */
+int main(void){
+	char alpha[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+		'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+		's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+	int i = 0;
+
+	while(i <26)
+	{
+		putchar(alpha[i]);
+		i++;
+	}
+
+	i = 0;
+
+	while(i <26)
+	{
+		putchar(toupper(alpha[i]));
+		i++;
+	}
+	
+	putchar('\n');
+	return (0);
+}
