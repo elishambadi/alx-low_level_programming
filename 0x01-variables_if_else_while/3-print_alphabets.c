339d787f4905903407291821eea857b5a34adae4
elishambadi <embadi43@gmail.com>
2022-04-08 15:30:17 +0000

    add task 3 code v1

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,13 +7,14 @@
  *Return: 0 to terminate programme
  *
  */
-int main(void){
+int main(void)
+{
 	char alpha[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
 		'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
 		's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 	int i = 0;
 
-	while(i <26)
+	while (i < 26)
 	{
 		putchar(alpha[i]);
 		i++;
@@ -21,12 +22,12 @@ int main(void){
 
 	i = 0;
 
-	while(i <26)
+	while (i < 26)
 	{
 		putchar(toupper(alpha[i]));
 		i++;
 	}
-	
+
 	putchar('\n');
 	return (0);
 }
