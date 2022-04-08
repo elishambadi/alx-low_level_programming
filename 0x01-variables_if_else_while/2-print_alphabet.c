0ff9ebf32921d9280ca571bd7c2d98028316ad4e
elishambadi <embadi43@gmail.com>
2022-04-08 15:05:34 +0000

    add while loop to print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ *main - main function
+ *
+ *Return: 0 to terminate program
+ */
+ 
+ int main(){
+	 char alpha[26] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+
+	 int i = 0;
+
+	 while (i<26){
+		char b = alpha[i];
+
+		printf("%c",b);
+		i++;
+	 }
+	 printf("\n");
+	 return (0);
+ } 
