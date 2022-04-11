449a40b80eeb7a73fe56a3e1efdf53509ab0a1f0
elishambadi <embadi43@gmail.com>
2022-04-11 05:49:09 +0000

    add task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ *main - main function
+ *
+ *Return: 0 to terminate program
+ *
+ */
+
+int main(void)
+{
+	int i = '0';
+	int j = '0';
+
+	while (i <= '9')
+	{
+		j = i + 1;
+		while (j <= '9')
+		{
+			putchar(i);
+			putchar(j);
+			if (i != '8'){
+				putchar(',');
+				putchar(' ');
+			}
+			j++;
+		}
+		i++;
+	}
+	putchar('\n');
+
+	return (0);
+}
