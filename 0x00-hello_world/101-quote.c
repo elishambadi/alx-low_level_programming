0cdbdc36e0467f89bf2ac9b50961584b32c02861
elishambadi <embadi43@gmail.com>
2022-04-11 05:32:00 +0000

    fix grammar erros

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,7 +9,7 @@
 
 int main(void)
 {
-	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-1-19 \n";
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19 \n";
 
 	fwrite(str, strlen(str), 1, stderr);
 
