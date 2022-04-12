3d3f2badad1950029b42676e0238bd3067b04a98
elishambadi <embadi43@gmail.com>
2022-04-12 22:34:05 +0000

    edit betty reqs -10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,33 +2,16 @@
 
 void _putchar(char c);
 
+/**
+ *add - add two numbers
+ *@a: number one
+ *@b: number two
+ *
+ *Return: sum of the two numbers
+ */
 int add(int a, int b)
 {
 	int sum = a + b;
-	return (sum);
-	/**
-	char one, two, three;
 
-	if (sum < 1000 && sum > 100)
-	{
-		one = (char)sum / 100;
-		two = (char)sum / 10;
-		three = (char)sum % 10;
-		_putchar(one);
-		_putchar(two);
-		_putchar(three);
-	}
-	else if (sum < 100 && sum >= 10)
-	{
-		two =(char) sum / 10;
-		three =(char) sum % 10;
-		_putchar(two);
-		_putchar(three);
-	}
-	else if (sum < 10)
-	{
-		three = (char) sum;
-		_putchar(three);
-	} 
-	*/
+	return (sum);
 }
