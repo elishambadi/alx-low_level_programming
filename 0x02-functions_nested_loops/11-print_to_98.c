e52eafaf39cc5f365a6846c8dfd11a2053875cd0
elishambadi <embadi43@gmail.com>
2022-04-12 22:29:30 +0000

    edit betty requirements

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,24 +2,37 @@
 
 void _putchar(char c);
 
-void print(int n){
+/**
+ *print - prints an integer
+ *&n - integer to print
+ *
+ *Return: void
+ */
+void print(int n)
+{
 	if (n < 0)
 	{
 		_putchar('-');
 		n = -n;
 	}
 
-	if (n/10)
+	if (n / 10)
 		print(n / 10);
 
 	_putchar(n % 10 + '0');
 }
 
+/**
+ *print_to_98 - print numbers to 98 forward or backward
+ *&n - starting integer
+ *
+ *Return: void
+ */
 void print_to_98(int n)
 {
 	if (n <= 98)
 	{
-		while(n <= 98)
+		while (n <= 98)
 		{
 			print(n);
 			if (n != 98)
