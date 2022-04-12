e15240440c6dd3689bc04d250ee9c739461fb801
elishambadi <embadi43@gmail.com>
2022-04-12 11:08:06 +0000

    add task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ *_putchar - prints a character
+ *&char c - character to be printed
+ *
+ *Return: void
+ */
+void _putchar(char c);
+
+/**
+ *_isalpha - checks the alphabet
+ *&int c - int variable
+ *Return: 0 to terminate program
+ */
+int _abs(int n)
+{
+	n = abs(n);
+	return (n);
+}
