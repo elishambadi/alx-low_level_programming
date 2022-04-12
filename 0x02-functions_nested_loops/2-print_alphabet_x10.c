5d78e4f62da501a19f745e1892d7e3c8435ea9fd
elishambadi <embadi43@gmail.com>
2022-04-12 23:16:41 +0000

    add print alphabet x10 v2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,24 +2,13 @@
 #include "main.h"
 
 /**
- *_putchar - prints a character
- *&char c - character to be printed
- *
- *Return: void
- */
-void _putchar(char c);
-
-/**
- *print_alphabet - prints the alphabet
- *
- *Return: 0 to terminate program
+ *print_alphabet_x10 - prints the alphabet
  */
 void print_alphabet_x10(void)
 {
 	char alpha[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
 		'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
 		's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
-
 	int i = 0;
 	int j = 0;
 
