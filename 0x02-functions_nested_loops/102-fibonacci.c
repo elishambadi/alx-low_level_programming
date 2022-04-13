9fc0f7916b672454593ff8fe7d3feaab6a02faed
elishambadi <embadi43@gmail.com>
2022-04-13 08:41:27 +0000

    add task 14

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ *main - print fobo sequence
+ *
+ *Return: 0 to terminate
+ */
+int main(void)
+{
+	long int n1 = 1, n2 = 2, i;
+	long int next = n1 + n2;
+
+	printf("%ld, %ld ,", n1, n2);
+
+	for (i = 3; i < 50; i++)
+	{
+		printf("%ld ,", next);
+		n1 = n2;
+		n2 = next;
+		next = n1 + n2;
+	}
+	return (0);
+}
