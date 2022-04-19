fc1bd0e0761832565ca7be726912b8c02edb4a6d
elishambadi <embadi43@gmail.com>
2022-04-19 10:20:58 +0000

    add task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *mul - multiply 2 nums
+ *
+ *@a: number 1
+ *@b: number 2
+ *
+ *Return: mul result
+ */
+
+int mul(int a, int b)
+{
+	int res = a * b;
+	return (res)
+}
