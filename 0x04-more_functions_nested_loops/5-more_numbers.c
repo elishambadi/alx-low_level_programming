341007f21dafe91f4c8bf0fe844a6e60b7b18a5d
elishambadi <embadi43@gmail.com>
2022-04-19 11:08:59 +0000

    add task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *more_numbers - print numbers 10 times
+ */
+
+void more_numbers(void)
+{
+	int i = '0';
+	int j = '0';
+
+	while (i <= '1')
+	{
+		while (j <= '9')
+		{
+			if (i == '1' && j == '5')
+			{
+				break;
+			}
+			else
+			{
+				if (i != '0')
+				{
+					_putchar(i);
+				}
+				_putchar(j);
+			}
+			j++;
+		}
+		j = '0';
+		i++;
+	}
+	_putchar('\n');
+}
