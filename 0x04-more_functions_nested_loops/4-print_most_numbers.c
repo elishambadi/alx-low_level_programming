4ae051b6b1da08826306110ec35e8e9fcf5fc446
elishambadi <embadi43@gmail.com>
2022-04-19 10:51:24 +0000

    add task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *print_most_numbers - prints numbers except 2 and 4
+ */
+
+void print_most_numbers(void)
+{
+	int i = '0';
+
+	while (i <= '9')
+	{
+		if (i != '2' &&	i != '4')
+		{
+			_putchar(i);
+		}
+		i++;
+	}
+	_putchar('\n');
+}
