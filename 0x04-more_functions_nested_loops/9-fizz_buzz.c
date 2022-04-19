727dfda99220c484f8aeaa567e0a0fe19e85759b
elishambadi <embadi43@gmail.com>
2022-04-19 14:44:30 +0000

    add task 9 fizz wqbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ *main - do fizz buzz test
+ *
+ *Return: 0 to terminate program
+ */
+
+int main(void)
+{
+	int i = 1;
+
+	while (i <= 100)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+		i++;
+	}
+
+	printf("\n");
+	return (0);
+}
