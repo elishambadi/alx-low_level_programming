4cf155bceca33ca14ac6f36d9f4cd0862629f23f
elishambadi <embadi43@gmail.com>
2022-04-20 14:48:26 +0000

    add task 0 v2

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,18 +1,16 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
  *main - main function
- *
  *Return: 0 to terminate
  */
 
 int main(void)
 {
-	int i = 0;
+	int i;
 
+	i = 0;
 	positive_or_negative(i);
-	printf("\n");
 
 	return (0);
 }
