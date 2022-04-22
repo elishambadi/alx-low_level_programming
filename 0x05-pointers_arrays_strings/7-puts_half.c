b19394ba843ea137d6eb9baf884de804a1cf5a79
elishambadi <embadi43@gmail.com>
2022-04-22 07:59:17 +0000

    add task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ *puts_half - reverse string
+ *@str: string to check
+ */
+
+void puts_half(char *str)
+{
+	int len = strlen(str), half_len;
+	int i = 0;
+
+	if (len % 2 != 0)
+	{
+		half_len = (len - 1) / 2 + 1;
+	}
+	else
+	{
+		half_len = len / 2;
+	}
+
+	for (i = half_len; i <= len; i++)
+	{
+		printf("%c", str[i]);
+	}
+	printf("\n");
+}
