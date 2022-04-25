8ea2fa6c65bcbe009ea14f404041983817eaf432
elishambadi <embadi43@gmail.com>
2022-04-25 06:35:22 +0000

    add task 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -15,18 +15,19 @@ char *leet(char *c)
 	int i, j;
 	int len = strlen(c);
 
-	char num[5] = {'4','3','0','7','1'};
-	char alpha[5] = {'a','e','o','t','l'};
+	char num[5] = {'4', '3', '0', '7', '1'};
+	char alpha[5] = {'a', 'e', 'o', 't', 'l'};
 
 	for (i = 0; i < len; i++)
 	{
 		for (j = 0; j < 5; j++)
 		{
-			if(*(c + i) == alpha[j] || *(c + i) == toupper(alpha[j]))
+			if (*(c + i) == alpha[j] || *(c + i) == toupper(alpha[j]))
 			{
 				*(c + i) = num[j];
 			}
 		}
+
 		j = 0;
 	}
 
