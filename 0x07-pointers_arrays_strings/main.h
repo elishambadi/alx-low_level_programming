97663fcf5bab63e5697420a9931ef1828bcab504
elishambadi <embadi43@gmail.com>
2022-04-26 22:44:48 +0000

    edit main.h file

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -6,9 +6,9 @@ char *_strchr(char *s, char c);
 
 unsigned int _strspn(char *s, char *accept);
 
-char *strpbrk(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
 
-char *strstr(char *haystack, char *needle);
+char *_strstr(char *haystack, char *needle);
 
 void print_chessboard(char (*a)[8]);
 
