71dcb98142497651db1bb2a98483f5b87928e42c
elishambadi <embadi43@gmail.com>
2022-04-26 23:13:52 +0000

    fix betty coding v1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include "main.h"
 
 /**
 *main - causes an infinite loop
@@ -14,10 +13,11 @@ int main(void)
 
 	i = 0;
 
-	/*while (i < 10)
-        *{
-	*	putchar(i);
-	*/
+	/**
+	 * while (i < 10)
+	 * {
+	 * 	putchar(i);
+	 */
 
 	printf("Infinite loop avoided! \\o/\n");
 
