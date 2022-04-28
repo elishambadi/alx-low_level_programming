8ccae4cde73f492f22410d210d6c11a1a80709b8
elishambadi <embadi43@gmail.com>
2022-04-28 20:59:50 +0000

    add task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,1 +1,22 @@
 #include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ *_puts_recursion - puts a string recursively
+ *@s: string
+ */
+
+void _puts_recursion(char *s)
+{
+	int len = strlen(s);
+	int i = 0;
+
+	while (i <= len)
+	{
+		_putchar(*(s + i));
+		i++;
+	}
+
+	_putchar('\n');
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,1 +1,3 @@
 void _puts_recursion(char *s);
+
+void _putchar(char c);
