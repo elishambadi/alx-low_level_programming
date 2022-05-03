a786059ac34fc749a25d0bb754c175dd08e16419
elishambadi <embadi43@gmail.com>
2022-05-03 09:33:09 +0000

    add task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *main - main function
+ *@argc: argc
+ *@argv: argv
+ *
+ *Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int argc1 __attribute__((unused)) = argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
