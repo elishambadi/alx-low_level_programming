b65e4f503546bdc20634761576746cb563ffa7c9
elishambadi <embadi43@gmail.com>
2022-05-05 10:02:28 +0000

    add task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *_strdup - pointer to a copied string
+ *@str: string
+ *
+ *Return: char array
+ */
+
+char *_strdup(char *str)
+{
+	char *str1;
+	unsigned int i;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	str1 = malloc(sizeof(char) * strlen(str));
+
+	for (i = 0; i < strlen(str); i++)
+	{
+		str1[i] = str[i];
+	}
+
+	return (str1);
+}
