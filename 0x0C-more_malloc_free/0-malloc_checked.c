ec2840ca0f54767a40f92a79fe2d5a861552421b
elishambadi <embadi43@gmail.com>
2022-05-11 04:38:34 +0000

    edit to meet betty guidelines

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -4,17 +4,26 @@
 /**
  *malloc_checked - check if malloc works
  *@b: size of mem
+ *Return: void pointer
  */
 
 void *malloc_checked(unsigned int b)
 {
 	int *mem;
+
 	mem = (void *) malloc(b);
 
 	if (mem == NULL)
 	{
 		exit(98);
 	}
+
+	/**
+	 *Note that a void * function
+	 *must return a void pointer as opposed
+	 *to an ordinary void that doesn't have to
+	 *return anything
+	 */
 
 	return (mem);
 }
