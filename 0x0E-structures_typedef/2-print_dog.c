a98a9ff7b318a64eef13215df03a9f5fc2580867
elishambadi <embadi43@gmail.com>
2022-05-13 18:17:00 +0000

    add null checkers

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,10 +1,26 @@
 #include <stdio.h>
 #include "dog.h"
+
 /**
  *print_dog - initialize
  *@d: dog struct
  */
+
 void print_dog(struct dog *d)
 {
-	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+	if (d != NULL)
+	{
+		char *name = d->name;
+		char *owner = d->owner;
+		
+		if (d->name == NULL)
+		{
+			name = "(nil)";
+		}
+		if (d->owner == NULL)
+		{
+			owner = "(nil)";
+		}
+		printf("Name: %s\nAge: %f\nOwner: %s\n", name, d->age, owner);
+	}
 }
