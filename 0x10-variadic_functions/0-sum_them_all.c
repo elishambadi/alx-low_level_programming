e82bbc16f0ec6c790b62246adbbfb02c242dfb0c
elishambadi <embadi43@gmail.com>
2022-05-17 09:53:03 +0000

    add include guards

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include <stdarg.h>
 #include "variadic_functions.h"
 
@@ -8,6 +7,7 @@
  *
  *Return: sum
  */
+
 int sum_them_all(const unsigned int n, ...)
 {
 	unsigned int i, sum;
@@ -18,6 +18,7 @@ int sum_them_all(const unsigned int n, ...)
 
 	va_start(ap, n);
 	sum = 0;
+
 	for (i = 0; i < n; i++)
 	{
 		sum += va_arg(ap, int);
