6c1d4782b6d2e08ee4e5bffa981afc20eb3425de
elishambadi <embadi43@gmail.com>
2022-05-17 09:49:48 +0000

    initial commit

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ *sum_them_all - sum all vars
+ *n - no of vars
+ *
+ *Return: sum
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i, sum;
+	va_list ap;
+
+	if (n == 0)
+		return (0);
+
+	va_start(ap, n);
+	sum = 0;
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(ap, int);
+	}
+	va_end(ap);
+
+	return (sum);
+}
---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,8 @@
+#Variadic Functions
+
+Variadic functions can accept an indefinite number of arguments. An example of an inbuilt one
+is printf() function.
+
+Use stdarg.h header file.
+
+Macros used are va_list, va_start, va_arg and va_end.
