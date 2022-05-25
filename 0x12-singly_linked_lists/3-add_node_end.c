383c4f2bf4e74aa66ec3ddfbd82294c5d2301d0a
elishambadi <embadi43@gmail.com>
2022-05-25 23:38:23 +0000

    rewrite task 3

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,40 +1,44 @@
-#include <stdio.h>
+#include <stddef.h>
 #include <stdlib.h>
 #include <string.h>
 #include "lists.h"
 
 /**
- *add_node_end - create and add a node at end
- *@head: list head pointer
- *@str: node value
- *
- *Return: new node
+ * add_node_end - adds a new node at the end
+ * of a list_t list.
+ * @head: head of the linked list.
+ * @str: string to store in the list.
+ * Return: address of the head.
  */
 
 list_t *add_node_end(list_t **head, const char *str)
 {
-	char *string;
-	unsigned int len;
-
-	list_t *node = NULL;
-
-	string = malloc(sizeof(str));
-	strcpy(string, str);
-
-	node = malloc(sizeof(list_t));
-
-	if (node == NULL)
-	{
+	list_t *new, *temp;
+	size_t nchar;
+	
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
 		return (NULL);
+	
+	new->str = strdup(str);
+	
+	for (nchar = 0; str[nchar]; nchar++)
+		;
+
+	new->len = nchar;
+	new->next = NULL;
+	temp = *head;
+
+	if (temp == NULL)
+	{
+		*head = new;
 	}
-
-	len = strlen(string);
-
-	node->str = string;
-	node->next = *head;
-	node->len = len;
-
-	printf("[%u] %s\n", node->len, node->str);
-
-	return (node);
+	else
+	{
+		while (temp->next != NULL)
+			temp = temp->next;
+		temp->next = new;
+	}
+	
+	return (*head);
 }
