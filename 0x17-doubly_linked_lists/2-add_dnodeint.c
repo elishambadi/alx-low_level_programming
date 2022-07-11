080bf81914bb435d8577011a2dc95600e5933d27
Elisha Mbadi <embadi43@gmail.com>
2022-07-11 15:03:23 +0300

    add null check

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -13,6 +13,11 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new_head = malloc(sizeof(dlistint_t));
 
+	if (new_head == NULL)
+	{
+		return NULL;
+	}
+
 	new_head->n = n;
 	new_head->prev = NULL;
 	new_head->next = *head;
