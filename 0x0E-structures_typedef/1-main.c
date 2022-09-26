ef4bf85b231b4ca683469665efaaa3fcf0bdf337
Elisha Mbadi <embadi43@gmail.com>
2022-09-26 15:16:35 +0300

    add null check

---
+++ 0x0E-structures_typedef/1-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    struct dog my_dog;
+
+    init_dog(NULL, "Poppy", 3.5, "Bob");
+    return (0);
+}
