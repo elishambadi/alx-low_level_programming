f77a434b4cd5565b6052a245d81246405f7e24b9
elishambadi <embadi43@gmail.com>
2023-01-13 15:00:09 +0300

    password found

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -4,7 +4,7 @@
 /**
  * get_endianness - checks the endianness.
  * What is endianess?
- * 
+ *
  * Attribute to check whether integers are represented by the MSB
  * stored at the lowest address(big endian) or at highest address
  * (little endian)
