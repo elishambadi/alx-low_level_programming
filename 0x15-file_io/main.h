388318d82c14d74eac267597aa15bb2a519cb26e
elishambadi <embadi43@gmail.com>
2023-01-13 20:23:59 +0300

    check arg no

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -12,5 +12,9 @@ size_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 size_t _strlen(char *str);
+void check100(int check, int fd);
+void check99(ssize_t check, char *file, int fd_from, int fd_to);
+void check98(ssize_t check, char *file, int fd_from, int fd_to);
+void check97(int argc);
 
 #endif
