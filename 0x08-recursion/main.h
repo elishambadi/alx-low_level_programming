2fff315aa52f5412f28b3045299f806aa063384d
elishambadi <embadi43@gmail.com>
2023-01-13 17:12:13 +0300

    add adv task 100

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 void _puts_recursion(char *s);
 
 void _putchar(char c);
@@ -13,3 +16,9 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 
 int is_prime_number(int n);
+
+int pal_checker(char *s, int i, int j);
+
+int is_palindrome(char *s);
+
+#endif
