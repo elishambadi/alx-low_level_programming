6d81730f4dd6e9af6c5e411073c5e53d4aa189fd
elishambadi <embadi43@gmail.com>
2023-01-13 14:58:26 +0300

    endianess finder

---
+++ 0x14-bit_manipulation/100-get_endianess.c
@@ -0,0 +1,23 @@
+
+
+#include "main.h"
+/**
+ * get_endianness - checks the endianness.
+ * What is endianess?
+ * 
+ * Attribute to check whether integers are represented by the MSB
+ * stored at the lowest address(big endian) or at highest address
+ * (little endian)
+ *
+ * Return: 0 if big endian, 1 if little endian
+ */
+int get_endianness(void)
+{
+	int i;
+	char *c;
+
+	i = 1;
+	c = (char *)&i;
+
+	return (*c);
+}
